1118f5d34b6baa3be307ec2cd02bbce371f3e012
Christophe Ninucci <christophe.ninucci@direccte.gouv.fr>
2020-08-26 16:29:19 +0200

    departements + regions + naf handlers

--- handlers.go
+++ handlers.go
@@ -208,18 +208,67 @@ func getListeScores(c *gin.Context) {
 }
 
 func getCodesNaf(c *gin.Context) {
-	log.Println("getCodesNaf")
-	c.JSON(200, "getCodesNaf")
+	rows, err := db.Query(context.Background(), "select code, libelle from naf where niveau=1")
+	if err != nil {
+		c.AbortWithError(500, err)
+		return
+	}
+	var naf = make(map[string]string)
+	for rows.Next() {
+		var code string
+		var libelle string
+		err := rows.Scan(&code, &libelle)
+		if err != nil {
+			c.AbortWithError(500, err)
+			return
+		}
+		naf[code] = libelle
+	}
+	c.JSON(200, naf)
 }
 
 func getDepartements(c *gin.Context) {
-	log.Println("getDepartements")
-	c.JSON(200, "getDepartements")
+	rows, err := db.Query(context.Background(), "select code, libelle from departements")
+	if err != nil {
+		c.AbortWithError(500, err)
+		return
+	}
+	var departements = make(map[string]string)
+	for rows.Next() {
+		var code string
+		var libelle string
+		err := rows.Scan(&code, &libelle)
+		if err != nil {
+			c.AbortWithError(500, err)
+			return
+		}
+		departements[code] = libelle
+	}
+	c.JSON(200, departements)
 }
 
 func getRegions(c *gin.Context) {
-	log.Println("getRegions")
-	c.JSON(200, "getRegions")
+	rows, err := db.Query(context.Background(), `select r.libelle, array_agg(d.code order by d.code) from regions r
+	inner join departements d on d.id_region = r.id
+	group by r.libelle`)
+
+	if err != nil {
+		c.AbortWithError(500, err)
+		return
+	}
+
+	var regions = make(map[string][]string)
+	for rows.Next() {
+		var region string
+		var departements []string
+		err := rows.Scan(&region, &departements)
+		if err != nil {
+			c.AbortWithError(500, err)
+			return
+		}
+		regions[region] = departements
+	}
+	c.JSON(200, regions)
 }
 
 func importHandler(c *gin.Context) {
