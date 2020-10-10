e056d75819371a395e03e8a87c09fdf478935424
chrnin <christophe.ninucci@direccte.gouv.fr>
2020-10-10 15:16:18 +0200

    update test files

--- test/test.sh
+++ test/test.sh
@@ -5,25 +5,34 @@
 # pour mettre l'environnement en attente et lancer les tests en parallèle, utilisez le flag -w
 # les flags ne sont pas cumulables
 
-cd ..
-go build
-build_state=$?
-if [ $build_state -ne 0 ]; then
-  echo "echec de la compilation, abandon"
-  exit $build_state
+set -e
+
+function cleanup()
+{
+if [ "$DATAPI_PID" != "" ]; then
+  echo "- arret datapi"
+  kill $DATAPI_PID;
 fi
 
-cd test 
+rm -rf workspace
 
+echo "- arret conteneur"
+docker stop "$POSTGRES_CONTAINER" > /dev/null 2>&1
+echo "- suppression conteneur"
+docker rm "$POSTGRES_CONTAINER" > /dev/null 2>&1
+}
+
+trap cleanup EXIT
+
+cd ..
+go build
+
+cd test 
 TEST_DATA="data/testData.sql.gz"
 POSTGRES_PASSWORD="test"
 POSTGRES_DOCKERPORT="65432"
 DATAPI_PORT="12345"
 
-test -x ../datapi || echo "exécutable datapi non présent, abandon"
-test -x ../datapi || exit
-
-echo "- exécutable datapi présent"
 echo "- initialisation docker postgres"
 POSTGRES_CONTAINER=$(docker run -e POSTGRES_PASSWORD="$POSTGRES_PASSWORD" -p "$POSTGRES_DOCKERPORT":5432 -d postgres:10-alpine)
 echo "- conteneur créé: $POSTGRES_CONTAINER"
@@ -31,13 +40,11 @@ sleep 4
 echo "- creation de la base de données"
 docker exec -i "$POSTGRES_CONTAINER" /usr/local/bin/createdb -U postgres datapi_test
 echo "- insert des données de test"
-zcat "$TEST_DATA" | docker exec -i "$POSTGRES_CONTAINER" /usr/local/bin/psql -U postgres datapi_test > /dev/null 2>&1
-
+zcat < "$TEST_DATA" | docker exec -i "$POSTGRES_CONTAINER" /usr/local/bin/psql -U postgres datapi_test > /dev/null 2>&1
 
 echo "- generation configuration de test"
 mkdir workspace
 sed "s/changemypass/$POSTGRES_PASSWORD/" config.toml.source | sed "s/changemyport/$DATAPI_PORT/" | sed "s/changemypgport/$POSTGRES_DOCKERPORT/" > workspace/config.toml
-
 
 cp ../datapi workspace
 cp -r ../migrations workspace
@@ -59,15 +66,3 @@ fi
 cd ../
 
 DATAPI_PORT="$DATAPI_PORT" GOLDEN_UPDATE="$GOLDEN_UPDATE" go test -v
-TEST_STATUS="$?"
-
-kill $DATAPI_PID
-rm -rf workspace
-
-# rm config.toml
-echo "- arret conteneur"
-docker stop "$POSTGRES_CONTAINER" > /dev/null 2>&1
-echo "- suppression conteneur"
-docker rm "$POSTGRES_CONTAINER" > /dev/null 2>&1
-
-exit $TEST_STATUS
