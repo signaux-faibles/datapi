4734c572a2162206533178d63ba6a0bc8370742b
Christophe Ninucci <christophe.ninucci@direccte.gouv.fr>
2020-11-19 03:25:59 +0100

    cast types + update tests

--- summary.go
+++ summary.go
@@ -8,45 +8,45 @@ import (
 
 // Summary score + élements sur l'établissement
 type Summary struct {
-	Siren                string             `json:"siren"`
-	Siret                string             `json:"siret"`
-	ValeurScore          *float64           `json:"-"`
-	DetailScore          map[string]float64 `json:"-"`
-	Diff                 *float64           `json:"-"`
-	RaisonSociale        *string            `json:"raison_sociale"`
-	Commune              *string            `json:"commune"`
-	LibelleActivite      *string            `json:"libelle_activite"`
-	LibelleActiviteN1    *string            `json:"libelle_activite_n1"`
-	CodeActivite         *string            `json:"code_activite"`
-	CodeDepartement      *string            `json:"departement"`
-	LibelleDepartement   *string            `json:"libelleDepartement"`
-	Effectif             *float64           `json:"dernier_effectif"`
-	MontantDetteUrssaf   *float64           `json:"montantDetteUrssaf,omitempty"`
-	HausseUrssaf         *bool              `json:"urssaf,omitempty"`
-	ActivitePartielle    *bool              `json:"activite_partielle,omitempty"`
-	APConsoHeureConsomme *float64           `json:"apconsoHeureConsomme,omitempty"`
-	APConsoMontant       *float64           `json:"apconsoMontant,omitempty"`
-	ChiffreAffaire       *float64           `json:"ca"`
-	VariationCA          *float64           `json:"variation_ca"`
-	ArreteBilan          *time.Time         `json:"arrete_bilan"`
-	ResultatExploitation *float64           `json:"resultat_expl"`
-	EtatProcol           *string            `json:"etat_procol,omitempty"`
-	Alert                *string            `json:"alert,omitempty"`
-	Visible              *bool              `json:"visible,omitempty"`
-	InZone               *bool              `json:"inZone,omitempty"`
-	Followed             *bool              `json:"followed,omitempty"`
-	FollowedEntreprise   *bool              `json:"followedEntreprise,omitempty"`
-	FirstAlert           *bool              `json:"firstAlert"`
-	Siege                *bool              `json:"siege"`
-	Groupe               *string            `json:"groupe,omitempty"`
-	TerrInd              *bool              `json:"territoireIndustrie,omitempty"`
-	PermUrssaf           *bool              `json:"permUrssaf,omitempty"`
-	PermDGEFP            *bool              `json:"permDGEFP,omitempty"`
-	PermScore            *bool              `json:"permScore,omitempty"`
-	PermBDF              *bool              `json:"permBDF,omitempty"`
-	Comment              *string            `json:"-"`
-	Category             *string            `json:"-"`
-	Since                *time.Time         `json:"-"`
+	Siren                 string             `json:"siren"`
+	Siret                 string             `json:"siret"`
+	ValeurScore           *float64           `json:"-"`
+	DetailScore           map[string]float64 `json:"-"`
+	Diff                  *float64           `json:"-"`
+	RaisonSociale         *string            `json:"raison_sociale"`
+	Commune               *string            `json:"commune"`
+	LibelleActivite       *string            `json:"libelle_activite"`
+	LibelleActiviteN1     *string            `json:"libelle_activite_n1"`
+	CodeActivite          *string            `json:"code_activite"`
+	CodeDepartement       *string            `json:"departement"`
+	LibelleDepartement    *string            `json:"libelleDepartement"`
+	Effectif              *float64           `json:"dernier_effectif"`
+	MontantDetteUrssaf    *float64           `json:"montantDetteUrssaf,omitempty"`
+	HausseUrssaf          *bool              `json:"urssaf,omitempty"`
+	ActivitePartielle     *bool              `json:"activite_partielle,omitempty"`
+	APHeureConsommeAVG12m *int               `json:"apHeureConsommeAVG12m,omitempty"`
+	APMontantAVG12m       *int               `json:"apMontantAVG12m,omitempty"`
+	ChiffreAffaire        *float64           `json:"ca"`
+	VariationCA           *float64           `json:"variation_ca"`
+	ArreteBilan           *time.Time         `json:"arrete_bilan"`
+	ResultatExploitation  *float64           `json:"resultat_expl"`
+	EtatProcol            *string            `json:"etat_procol,omitempty"`
+	Alert                 *string            `json:"alert,omitempty"`
+	Visible               *bool              `json:"visible,omitempty"`
+	InZone                *bool              `json:"inZone,omitempty"`
+	Followed              *bool              `json:"followed,omitempty"`
+	FollowedEntreprise    *bool              `json:"followedEntreprise,omitempty"`
+	FirstAlert            *bool              `json:"firstAlert"`
+	Siege                 *bool              `json:"siege"`
+	Groupe                *string            `json:"groupe,omitempty"`
+	TerrInd               *bool              `json:"territoireIndustrie,omitempty"`
+	PermUrssaf            *bool              `json:"permUrssaf,omitempty"`
+	PermDGEFP             *bool              `json:"permDGEFP,omitempty"`
+	PermScore             *bool              `json:"permScore,omitempty"`
+	PermBDF               *bool              `json:"permBDF,omitempty"`
+	Comment               *string            `json:"-"`
+	Category              *string            `json:"-"`
+	Since                 *time.Time         `json:"-"`
 }
 
 type summaries struct {
@@ -85,8 +85,8 @@ func (summaries *summaries) newSummary() []interface{} {
 		&s.CodeActivite,
 		&s.EtatProcol,
 		&s.ActivitePartielle,
-		&s.APConsoHeureConsomme,
-		&s.APConsoMontant,
+		&s.APHeureConsommeAVG12m,
+		&s.APMontantAVG12m,
 		&s.HausseUrssaf,
 		&s.Alert,
 		&summaries.global.count,
@@ -166,13 +166,11 @@ func (p summaryParams) toSQLParams() []interface{} {
 func getSummaries(params summaryParams) (summaries, error) {
 	var sql string
 
-	// if params.orderBy == "score" {
-	// 	sql = `select * from get_score($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18) as scores;`
-	// } else {
-	// 	sql = fmt.Sprintf("select * from get_summary($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18) as %s;", params.orderBy)
-	// }
-
-	sql = fmt.Sprintf("select * from get_summary($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18) as %s;", params.orderBy)
+	if params.orderBy == "score" {
+		sql = `select * from get_score($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18) as scores;`
+	} else {
+		sql = fmt.Sprintf("select * from get_summary($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18) as %s;", params.orderBy)
+	}
 
 	rows, err := db.Query(context.Background(), sql, params.toSQLParams()...)
 	if err != nil {
