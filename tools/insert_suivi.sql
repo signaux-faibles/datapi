96ee9def279fb77d2d10ef89aba1fd228e4bbc2c
Christophe Ninucci <christophe.ninucci@direccte.gouv.fr>
2021-01-19 10:48:28 +0100

    script semimanuel d'insert d'entreprises à suivre

---
+++ tools/insert_suivi.sql
@@ -0,0 +1,45 @@
+-- insert des siren à suivre
+drop table if exists insert_entreprise_suivi;
+create temporary table insert_entreprise_suivi as 
+select '123456789' as siren union
+select '234567890' as siren union
+select ...
+
+-- insert des sirets à suivre
+drop table if exists insert_etablissement_suivi;
+create temporary table insert_etablissement_suivi as
+select '12345678901234' as siret union
+select '23456789012345' as siret union
+select ...
+
+-- détection des établissements/entreprises absents
+select 'etablissement manquant' as motif, i.siret from insert_etablissement_suivi i
+left join etablissement0 e on e.siret = i.siret
+where e.id is null
+union
+select 'entreprise manquante', i.siren from insert_entreprise_suivi i
+left join entreprise0 e on e.siren = i.siren
+where e.id is null
+
+-- établissement sélectionné pour les entreprises, détection éventuelle des sieges manquants
+select i.siren, 
+coalesce(case when en.siren is null then 'entreprise manquante' end,
+		 case when e.siret is null then 'pas de siege pour cette entreprise' end,
+		 e.siret)
+from insert_entreprise_suivi i
+left join entreprise0 en on en.siren = i.siren
+left join etablissement0 e on e.siren = i.siren and e.siege
+
+
+-- ajout du suivi des établissements qui ne sont pas encore (ou plus) suivis pour emmanuel.lemaux
+with to_follow as (
+select s.siret from insert_etablissement_suivi s
+inner join etablissement e on e.siret = s.siret
+union select e.siret from insert_entreprise_suivi s
+inner join etablissement e on e.siren = s.siren and e.siege)
+insert into etablissement_follow (siret, siren, username, active, since, comment, category)
+select t.siret, substring(t.siret from 1 for 9),
+'email@user', true, current_timestamp, 'Suivi extrait du fichier de suivi CRP', 'fichier_CRP'
+from to_follow t
+left join etablissement_follow f on f.siret = t.siret and f.username = 'email@user' and until is null and active
+where f.id is null
