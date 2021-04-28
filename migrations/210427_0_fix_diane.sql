85f6752ac53e942c08ee18b9a0d7804ebe42d7ee
Christophe Ninucci <christophe.ninucci@direccte.gouv.fr>
2021-04-28 11:45:57 +0200

    add fields

---
+++ migrations/210427_0_fix_diane.sql
@@ -0,0 +1,6 @@
+alter table entreprise_diane add benefice_ou_perte real;
+alter table entreprise_diane add ca_par_effectif real;
+alter table entreprise_diane add taux_marge_commerciale real;
+alter table entreprise_diane add concours_bancaire_courant real;
+alter table entreprise_diane add effectif_consolide real;
+alter table entreprise_diane add efficacite_economique real;
