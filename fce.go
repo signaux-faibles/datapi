dbf4de8913d52e8190d2738fdc6751db6b5e11dd
fcoufour <github@coufourier.com>
2021-06-09 11:27:13 +0200

    feat: ajout de paramètres anchor et username pour FCE (#76)

--- fce.go
+++ fce.go
@@ -9,6 +9,12 @@ import (
 	"net/url"
 	"strconv"
 	"time"
+	"io"
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"encoding/hex"
+	"fmt"
 )
 
 // Fce type pour l'API Fiche Commune Entreprise
@@ -57,14 +63,65 @@ func getFceURL(c *gin.Context) {
 		c.JSON(500, err.Error())
 		return
 	}
-	fceVisitURL := makeFceVisitURL(siret, fce.Credential)
+	username := c.GetString("username")
+	fceVisitURL, err := makeFceVisitURL(siret, fce.Credential, username)
+	if err != nil {
+		c.JSON(500, err.Error())
+		return
+	}
 	c.String(200, fceVisitURL)
 }
 
-func makeFceVisitURL(siret string, credential string) (string) {
+func encrypt(stringToEncrypt string, key string) (string, error) {
+	bytesToEncrypt := []byte(stringToEncrypt)
+	keyBytes := []byte(key)
+	cipherBlock, err := aes.NewCipher(keyBytes)
+	if err != nil {
+		return "", err
+	}
+	aesGCM, err := cipher.NewGCM(cipherBlock)
+	if err != nil {
+		return "", err
+	}
+	nonce := make([]byte, aesGCM.NonceSize())
+	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
+		return "", err
+	}
+	encryptedBytes := aesGCM.Seal(nonce, nonce, bytesToEncrypt, nil)
+	return fmt.Sprintf("%x", encryptedBytes), nil
+}
+
+func decrypt(encryptedString string, key string) (string, error) {
+	encryptedBytes, _ := hex.DecodeString(encryptedString)
+	keyBytes := []byte(key)
+	cipherBlock, err := aes.NewCipher(keyBytes)
+	if err != nil {
+		return "", err
+	}
+	aesGCM, err := cipher.NewGCM(cipherBlock)
+	if err != nil {
+		return "", err
+	}
+	nonceSize := aesGCM.NonceSize()
+	nonce, data := encryptedBytes[:nonceSize], encryptedBytes[nonceSize:]
+	decryptedBytes, err := aesGCM.Open(nil, nonce, data, nil)
+	if err != nil {
+		return "", err
+	}
+	return fmt.Sprintf("%s", decryptedBytes), nil
+}
+
+func makeFceVisitURL(siret string, credential string, username string) (string, error) {
 	fceVisitURL := viper.GetString("fceVisitURL") + "establishment/" + siret
 	params := url.Values{}
+	params.Add("anchor", "direccte")
 	params.Add("credential", credential)
+	apiKey := viper.GetString("apiFceKey")
+	encryptedUsername, err := encrypt(username, apiKey)
+	params.Add("username", encryptedUsername)
+	if err != nil {
+		return "", err
+	}
 	fceVisitURL +=  "?" + params.Encode()
-	return fceVisitURL
+	return fceVisitURL, nil
 }
