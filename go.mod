88b2577ac1c301ca13d2402acc53cdba0efcbdf8
Christophe Ninucci <christophe.ninucci@direccte.gouv.fr>
2021-09-20 08:28:32 +0200

    intègre geosirene

--- go.mod
+++ go.mod
@@ -4,37 +4,42 @@ go 1.14
 
 require (
 	github.com/Nerzal/gocloak/v6 v6.4.0
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/cnf/structhash v0.0.0-20201127153200-e1b16c1ebc08
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
-	github.com/fsnotify/fsnotify v1.4.9 // indirect
+	github.com/fsnotify/fsnotify v1.5.1 // indirect
 	github.com/gin-contrib/cors v1.3.1
-	github.com/gin-gonic/gin v1.6.3
+	github.com/gin-gonic/gin v1.7.4
 	github.com/globalsign/mgo v0.0.0-20181015135952-eeefdecb41b8
-	github.com/go-playground/validator/v10 v10.4.0 // indirect
-	github.com/jackc/pgx/v4 v4.9.0
-	github.com/lib/pq v1.8.0
-	github.com/magiconair/properties v1.8.4 // indirect
-	github.com/mitchellh/mapstructure v1.3.3 // indirect
-	github.com/pelletier/go-toml v1.8.1 // indirect
+	github.com/go-playground/validator/v10 v10.9.0 // indirect
+	github.com/go-resty/resty/v2 v2.6.0 // indirect
+	github.com/go-stack/stack v1.8.1 // indirect
+	github.com/golang/snappy v0.0.4 // indirect
+	github.com/jackc/pgx/v4 v4.13.0
+	github.com/jackc/puddle v1.1.4 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/klauspost/compress v1.13.5 // indirect
+	github.com/lib/pq v1.10.3
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/mitchellh/mapstructure v1.4.2 // indirect
+	github.com/pelletier/go-toml v1.9.4 // indirect
 	github.com/pmezard/go-difflib v1.0.0
-	github.com/prometheus/client_golang v1.7.1
-	github.com/segmentio/ksuid v1.0.3 // indirect
-	github.com/signaux-faibles/goSirene v0.0.0-20210907133104-8f1612152fe5 // indirect
-	github.com/spf13/afero v1.4.1 // indirect
-	github.com/spf13/cast v1.3.1 // indirect
-	github.com/spf13/jwalterweatherman v1.1.0 // indirect
-	github.com/spf13/pflag v1.0.5 // indirect
-	github.com/spf13/viper v1.7.1
+	github.com/prometheus/client_golang v1.11.0
+	github.com/prometheus/common v0.30.0 // indirect
+	github.com/prometheus/procfs v0.7.3 // indirect
+	github.com/segmentio/ksuid v1.0.4 // indirect
+	github.com/signaux-faibles/goSirene v0.2.1
+	github.com/spf13/cast v1.4.1 // indirect
+	github.com/spf13/viper v1.8.1
 	github.com/tealeg/xlsx v1.0.5
-	go.mongodb.org/mongo-driver v1.5.3
-	golang.org/x/crypto v0.0.0-20201002170205-7f63de1d35b0 // indirect
-	golang.org/x/net v0.0.0-20201010224723-4f7140c49acb // indirect
-	golang.org/x/sys v0.0.0-20201009025420-dfb3f7c4e634 // indirect
-	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
-	google.golang.org/protobuf v1.25.0 // indirect
-	gopkg.in/ini.v1 v1.62.0 // indirect
-	gopkg.in/yaml.v2 v2.3.0 // indirect
-	modernc.org/b v1.0.0
-	modernc.org/mathutil v1.1.1 // indirect
-	modernc.org/strutil v1.1.0 // indirect
+	github.com/ugorji/go v1.2.6 // indirect
+	github.com/youmark/pkcs8 v0.0.0-20201027041543-1326539a0a0a // indirect
+	go.mongodb.org/mongo-driver v1.7.2
+	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
+	golang.org/x/net v0.0.0-20210913180222-943fd674d43e // indirect
+	golang.org/x/sys v0.0.0-20210910150752-751e447fb3d0 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
+	gopkg.in/ini.v1 v1.63.0 // indirect
+	modernc.org/b v1.0.2
 )
--- sirene.go
+++ sirene.go
@@ -3,12 +3,12 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
 	"os"
-	"strings"
 	"sync"
-	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v4"
 	"github.com/signaux-faibles/goSirene"
 	"github.com/spf13/viper"
 )
@@ -19,107 +19,387 @@ func sireneImportHandler(c *gin.Context) {
 		return
 	}
 	wg := sync.WaitGroup{}
-	wg.Add(1)
+	wg.Add(2)
 	ctx, cancelCtx := context.WithCancel(context.Background())
-	//go upsertSireneUL(ctx, cancelCtx, &wg)
+	go upsertSireneUL(ctx, cancelCtx, &wg)
 	go upsertGeoSirene(ctx, cancelCtx, &wg)
-	time.Sleep(time.Second * 5)
-	cancelCtx()
+
 	wg.Wait()
 }
 
 func upsertGeoSirene(ctx context.Context, cancelCtx context.CancelFunc, wg *sync.WaitGroup) {
-	file, err := os.Open(viper.GetString("sireneULPath"))
+	file, err := os.Open(viper.GetString("geoSirenePath"))
 	if err != nil {
 		cancelCtx()
-		return
 	}
 	geoSirene := goSirene.GeoSireneParser(ctx, file)
-	for i := range geoSirene {
-		fmt.Println(i)
+	count := 0
+	var batch []goSirene.GeoSirene
+	for sirene := range geoSirene {
+		count++
+		if count == 100000 {
+			count = 0
+			err := sqlGeoSirene(ctx, batch)
+			if err != nil {
+				cancelCtx()
+				fmt.Println(err.Error())
+				continue
+			}
+			log.Println("upsert GeoSirene")
+		}
+		if sirene.Error() == nil {
+			batch = append(batch, sirene)
+		}
+	}
+	err = sqlGeoSirene(ctx, batch)
+	if err != nil {
+		cancelCtx()
+		fmt.Println(err.Error())
 	}
 	wg.Done()
+}
+
+func sqlGeoSirene(ctx context.Context, data []goSirene.GeoSirene) error {
+	tx, err := db.BeginTx(ctx, pgx.TxOptions{})
+	if err != nil {
+		return err
+	}
+	tx.Exec(ctx, `
+	create temporary table tmp_geosirene 
+	(siret text, 
+		siren text, 
+		siege bool, 
+		creation timestamp, 
+		complement_adresse text, 
+		numero_voie text,
+		indice_repetition text, 
+		type_voie text, 
+		voie text, 
+		commune text, 
+		commune_etranger text, 
+		distribution_speciale text,
+		code_commune text, 
+		code_cedex text, 
+		cedex text, 
+		code_pays_etranger text, 
+		pays_etranger text, 
+		code_postal text,
+		departement text, 
+		code_activite text, 
+		nomen_activite text, 
+		latitude real, 
+		longitude real, 
+		tranche_effectif text, 
+		annee_effectif text, 
+		code_activite_registre_metiers text,
+		etat_administratif text, 
+		enseigne text, 
+		denomination_usuelle text, 
+		caractere_employeur bool)
+	on commit drop;`)
+
+	batch := &pgx.Batch{}
+	sql := `
+	  insert into tmp_geosirene (siret, siren, siege, creation, complement_adresse, numero_voie,
+			indice_repetition, type_voie, voie, commune, commune_etranger, distribution_speciale,
+			code_commune, code_cedex, cedex, code_pays_etranger, pays_etranger, code_postal,
+			departement, code_activite, nomen_activite, latitude, longitude, 
+		  tranche_effectif, annee_effectif, code_activite_registre_metiers,
+			etat_administratif, enseigne, denomination_usuelle, caractere_employeur) values 
+		($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,
+		 $12,$13,$14,$15,$16,$17,$18,$19,$20,
+		 $21,$22,$23,$24,$25,$26,$27,$28,$29,$30);`
+	for _, sirene := range data {
+		d := geoSireneData(sirene)
+		batch.Queue(sql, d...)
+	}
+	batch.Queue(`
+	  update etablissement e set 
+		siren = t.siren,
+		siege = t.siege,
+		creation = t.creation,
+		complement_adresse = t.complement_adresse,
+		numero_voie = t.numero_voie,
+		indice_repetition = t.indice_repetition,
+		type_voie = t.type_voie,
+		voie = t.voie,
+		commune = t.commune,
+		commune_etranger = t.commune_etranger,
+		distribution_speciale = t.distribution_speciale,
+		code_commune = t.code_commune,
+		code_cedex = t.code_cedex,
+		cedex = t.cedex,
+		code_pays_etranger = t.code_pays_etranger,
+		pays_etranger = t.pays_etranger,
+		code_postal = t.code_postal,
+		departement = t.departement,
+		code_activite = t.code_activite,
+		nomen_activite = t.nomen_activite,
+		latitude = t.latitude,
+		longitude = t.longitude,
+	  tranche_effectif = t.tranche_effectif,
+		annee_effectif = t.annee_effectif,
+		code_activite_registre_metiers = t.code_activite_registre_metiers,
+		etat_administratif = t.etat_administratif,
+		enseigne = t.enseigne,
+		denomination_usuelle = t.denomination_usuelle,
+		caractere_employeur = t.caractere_employeur
+		from tmp_geosirene t where t.siret = e.siret and e.version = 0;
+	`)
+	batch.Queue(`
+	  insert into etablissement
+		( siret, siren, siege, creation, complement_adresse, numero_voie,
+			indice_repetition, type_voie, voie, commune, commune_etranger, distribution_speciale,
+			code_commune, code_cedex, cedex, code_pays_etranger, pays_etranger, code_postal,
+			departement, code_activite, nomen_activite, latitude, longitude, 
+		  tranche_effectif, annee_effectif, code_activite_registre_metiers,
+			etat_administratif, enseigne, denomination_usuelle, caractere_employeur) select
+			s.siret,
+			s.siren,
+			s.siege,
+			s.creation,
+			s.complement_adresse,
+			s.numero_voie,
+			s.indice_repetition,
+			s.type_voie,
+			s.voie,
+			s.commune,
+			s.commune_etranger,
+			s.distribution_speciale,
+			s.code_commune,
+			s.code_cedex,
+			s.cedex,
+			s.code_pays_etranger,
+			s.pays_etranger,
+			s.code_postal,
+			s.departement,
+			s.code_activite,
+			s.nomen_activite,
+			s.latitude,
+			s.longitude,
+			s.tranche_effectif,
+			s.annee_effectif,
+			s.code_activite_registre_metiers,
+			s.etat_administratif,
+			s.enseigne,
+			s.denomination_usuelle,
+			s.caractere_employeur
+		from tmp_geosirene s
+		left join entreprise e on e.siren = s.siren and e.version = 0
+		where e.siren is null
+	`)
+	results := tx.SendBatch(ctx, batch)
+	err = results.Close()
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	err = tx.Commit(context.Background())
+	return err
 }
 
 func upsertSireneUL(ctx context.Context, cancelCtx context.CancelFunc, wg *sync.WaitGroup) {
 	sireneUL := goSirene.SireneULParser(ctx, viper.GetString("sireneULPath"))
 	count := 0
+	var batch []goSirene.SireneUL
 	for sirene := range sireneUL {
 		count++
-		fmt.Println(sirene)
-		if count == 5000 {
-			// fmt.Println(getInsertSireneUL(5))
+		if count == 100000 {
 			count = 0
+			err := sqlSireneUL(ctx, batch)
+			if err != nil {
+				cancelCtx()
+				fmt.Println(err.Error())
+				continue
+			}
+			log.Println("upsert SireneUL")
+		}
+		if sirene.Error() == nil {
+			batch = append(batch, sirene)
 		}
 	}
+	err := sqlSireneUL(ctx, batch)
+	if err != nil {
+		cancelCtx()
+		fmt.Println(err.Error())
+	}
 	wg.Done()
 }
 
-func getInsertSireneUL(length int) [2]string {
-	var create = `
-		create temporary table tmp_sirene_ul 
-		(siren text,
-		siret_siege text, 
-		raison_sociale text, 
-		prenom1 text, 
-		prenom2 text, 
-		prenom3 text, 
-		prenom4 text, 
-		nom text,
-		nom_usage text, 
-		statut_juridique text, 
-		creation date, 
-		sigle text, 
-		identification_association text, 
-		tranche_effetif text,
-		annee_effectif text, 
-		categorie text, 
-		annee_categorie text, 
-		etat_administratif text, 
-		nomusage text, 
-		denomination text, 
-		economie_sociale_solidaire boolean,
-		caractere_employeur boolean)
-		on commit drop;`
-	sql := `insert into tmp_sirene_url (siren text,
-		siret_siege text, 
-		raison_sociale text, 
-		prenom1 text, 
-		prenom2 text, 
-		prenom3 text, 
-		prenom4 text, 
-		nom text,
-		nom_usage text, 
-		statut_juridique text, 
-		creation date, 
-		sigle text, 
-		identification_association text, 
-		tranche_effetif text,
-		annee_effectif text, 
-		categorie text, 
-		annee_categorie text, 
-		etat_administratif text, 
-		nomusage text, 
-		denomination text, 
-		economie_sociale_solidaire boolean,
-		caractere_employeur boolean) values `
-	values := []string{}
-	for i := 0; i < length; i++ {
-		anchors := ids(i, 22)
-		values = append(values, fmt.Sprintf("($%d,$%d,$%d,$%d,$%d,$%d,$%d,$%d,$%d,$%d,$%d,$%d,$%d,$%d,$%d,$%d,$%d,$%d,$%d,$%d,$%d,$%d)", anchors...))
+func sqlSireneUL(ctx context.Context, data []goSirene.SireneUL) error {
+	tx, err := db.BeginTx(ctx, pgx.TxOptions{})
+	if err != nil {
+		return err
 	}
-	sql = sql + strings.Join(values, ",")
-	return [2]string{create, sql}
+	tx.Exec(ctx, `
+	create temporary table tmp_sirene_ul 
+	(siren text,
+	siret_siege text, 
+	raison_sociale text, 
+	prenom1 text, 
+	prenom2 text, 
+	prenom3 text, 
+	prenom4 text, 
+	nom text,
+	nom_usage text, 
+	statut_juridique text, 
+	creation date, 
+	sigle text, 
+	identifiant_association text, 
+	tranche_effectif text,
+	annee_effectif text, 
+	categorie text, 
+	annee_categorie text, 
+	etat_administratif text, 
+	economie_sociale_solidaire boolean,
+	caractere_employeur boolean)
+	on commit drop;`)
+
+	batch := &pgx.Batch{}
+	sql := `
+	  insert into tmp_sirene_ul (siren,
+		siret_siege, 
+		raison_sociale, 
+		prenom1, 
+		prenom2, 
+		prenom3, 
+		prenom4, 
+		nom,
+		nom_usage, 
+		statut_juridique, 
+		creation, 
+		sigle, 
+		identifiant_association, 
+		tranche_effectif,
+		annee_effectif, 
+		categorie, 
+		annee_categorie, 
+		etat_administratif, 
+		economie_sociale_solidaire,
+		caractere_employeur) values 
+		($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,
+		 $12,$13,$14,$15,$16,$17,$18,$19,$20);`
+	for _, sirene := range data {
+		d := sireneULdata(sirene)
+		batch.Queue(sql, d...)
+	}
+	batch.Queue(`
+	  update entreprise e set 
+		siret_siege = t.siret_siege,
+		raison_sociale = t.raison_sociale,
+		prenom1 = t.prenom1,
+		prenom2 = t.prenom2,
+		prenom3 = t.prenom3,
+		prenom4 = t.prenom4,
+		nom = t.nom,
+		nom_usage = t.nom_usage,
+		statut_juridique = t.statut_juridique,
+		creation = t.creation,
+		sigle = t.sigle,
+		identifiant_association = t.identifiant_association,
+		tranche_effectif = t.tranche_effectif,
+		annee_effectif = t.annee_effectif,
+		categorie = t.categorie,
+		annee_categorie = t.annee_categorie,
+		etat_administratif = t.etat_administratif,
+		economie_sociale_solidaire = t.economie_sociale_solidaire
+		from tmp_sirene_ul t where t.siren = e.siren and e.version = 0;
+	`)
+	batch.Queue(`
+	  insert into entreprise
+		(siren, siret_siege, raison_sociale, prenom1, prenom2, prenom3,
+			prenom4, nom, nom_usage, statut_juridique, creation, sigle,
+			identifiant_association, tranche_effectif, annee_effectif,
+			categorie, annee_categorie, etat_administratif,
+		  economie_sociale_solidaire, caractere_employeur) select
+		ul.siren,
+		ul.siret_siege,
+		ul.raison_sociale,
+		ul.prenom1,
+		ul.prenom2,
+		ul.prenom3,
+		ul.prenom4,
+		ul.nom,
+		ul.nom_usage,
+		ul.statut_juridique,
+		ul.creation,
+		ul.sigle,
+		ul.identifiant_association,
+		ul.tranche_effectif,
+		ul.annee_effectif,
+		ul.categorie,
+		ul.annee_categorie,
+		ul.etat_administratif,
+		ul.economie_sociale_solidaire,
+		ul.caractere_employeur
+		from tmp_sirene_ul ul
+		left join entreprise e on e.siren = ul.siren and e.version = 0
+		where e.siren is null
+	`)
+	results := tx.SendBatch(ctx, batch)
+	err = results.Close()
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	err = tx.Commit(context.Background())
+	return err
 }
 
-func ids(page int, length int) []interface{} {
-	r := []interface{}{}
-	for i := page*length + 1; i <= (page+1)*length; i++ {
-		r = append(r, i)
+func sireneULdata(s goSirene.SireneUL) []interface{} {
+	return []interface{}{
+		s.Siren,
+		s.NicSiegeUniteLegale,
+		s.RaisonSociale(),
+		s.Prenom1UniteLegale,
+		s.Prenom2UniteLegale,
+		s.Prenom3UniteLegale,
+		s.Prenom4UniteLegale,
+		s.NomUniteLegale,
+		s.NomUsageUniteLegale,
+		s.CategorieJuridiqueUniteLegale,
+		s.DateCreationUniteLegale,
+		s.SigleUniteLegale,
+		s.IdentifiantAssociationUniteLegale,
+		s.TrancheEffectifsUniteLegale,
+		s.AnneeEffectifsUniteLegale,
+		s.CategorieEntreprise,
+		s.AnneeCategorieEntreprise,
+		s.EtatAdministratifUniteLegale,
+		s.EconomieSocialeSolidaireUniteLegale,
+		s.CaractereEmployeurUniteLegale,
 	}
-	return r
 }
 
-func insertGeoSirene(ctx context.Context, cancelCtx context.CancelFunc, wg sync.WaitGroup, geoSirene chan goSirene.GeoSirene) {
-	wg.Done()
+func geoSireneData(s goSirene.GeoSirene) []interface{} {
+	return []interface{}{
+		s.Siret,
+		s.Siren,
+		s.EtablissementSiege,
+		s.DateCreationEtablissement,
+		s.ComplementAdresseEtablissement,
+		s.NumeroVoieEtablissement,
+		s.IndiceRepetitionEtablissement,
+		s.TypeVoieEtablissement,
+		s.LibelleVoieEtablissement,
+		s.LibelleCommuneEtablissement,
+		s.LibelleCommuneEtrangerEtablissement,
+		s.DistributionSpecialeEtablissement,
+		s.CodeCommuneEtablissement,
+		s.CodeCedexEtablissement,
+		s.LibelleCedexEtablissement,
+		s.CodePaysEtrangerEtablissement,
+		s.LibellePaysEtrangerEtablissement,
+		s.CodePostalEtablissement,
+		s.CodeDepartement(),
+		s.ActivitePrincipaleEtablissement,
+		s.NomenclatureActivitePrincipaleEtablissement,
+		s.Latitude,
+		s.Longitude,
+		s.TrancheEffectifsEtablissement,
+		s.AnneeEffectifsEtablissement,
+		s.ActivitePrincipaleRegistreMetiersEtablissement,
+		s.EtatAdministratifEtablissement,
+		s.Enseigne1Etablissement,
+		s.DenominationUsuelleEtablissement,
+		s.CaractereEmployeurEtablissement,
+	}
 }
