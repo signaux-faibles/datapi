7fc80dde45497fe00d01e74941ee25262306eac1
Christophe Ninucci <christophe-gitkraken@zbouboutchi.net>
2023-04-09 02:44:10 +0200

    add Slug & Users in kanbanConfig

--- src/core/wekan_config.go
+++ src/core/wekan_config.go
@@ -75,7 +75,7 @@ func buildWekanConfigPipeline() []bson.M {
 		"$match": bson.M{
 			"slug": bson.M{
 				"$regex": primitive.Regex{
-					Pattern: "^tableau-(crp|darp|codefi).*",
+					Pattern: "^tableau-(crp|eti|darp|codefi).*",
 					Options: "i",
 				}}}}
 
