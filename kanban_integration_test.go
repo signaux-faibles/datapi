b85594d3e5e2110ff3d9eeddcadec749a313cee4
Raphaël Squelbut <raphael.squelbut@shodo.io>
2023-04-21 17:39:28 +0200

    améliore le test de configuration du handler kanban

--- kanban_integration_test.go
+++ kanban_integration_test.go
@@ -3,16 +3,53 @@
 package main
 
 import (
+	"github.com/jaswdr/faker"
 	"github.com/signaux-faibles/datapi/src/test"
+	"github.com/spf13/viper"
 	"github.com/stretchr/testify/assert"
 	"net/http"
 	"testing"
 )
 
-func TestApi_Wekan_endpoint_is_configured(t *testing.T) {
-	ass := assert.New(t)
-	path := "/kanban/config"
-	response := test.HTTPGet(t, path)
+var fake faker.Faker
 
-	ass.Equal(http.StatusForbidden, response.StatusCode)
+func init() {
+	fake = faker.New()
+}
+
+func TestApi_Wekan_endpoint(t *testing.T) {
+
+	someRoles := []string{fake.App().Name(), fake.App().Name(), fake.App().Name()}
+	someRolesWithKanban := append(someRoles, "kanban")
+
+	type args struct {
+		roles []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"l'utilisateur n'a pas le rôle kanban", args{someRoles}, http.StatusForbidden},
+		{"l'utilisateur a le rôle kanban", args{someRolesWithKanban}, http.StatusOK},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ass := assert.New(t)
+			changeRoles(t, tt.args.roles...)
+			response := test.HTTPGet(t, "/kanban/config")
+			ass.Equal(tt.want, response.StatusCode)
+		})
+	}
+
+}
+
+func changeRoles(t *testing.T, roles ...string) {
+	// give kanban role
+	rolesBackup := viper.Get("fakeKeycloakRoles")
+	t.Cleanup(func() {
+		t.Logf("rollback des roles après le test '%s' : %s", t.Name(), rolesBackup)
+		viper.Set("fakeKeycloakRoles", rolesBackup)
+	})
+	viper.Set("fakeKeycloakRoles", roles)
 }
