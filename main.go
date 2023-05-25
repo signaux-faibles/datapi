917dd463bb4e3bb15a27a67967a2be44c0388f61
Christophe Ninucci <christophe-gitkraken@zbouboutchi.net>
2023-05-25 13:43:24 +0200

    - create tables - creation du package campaign - instancie le handler campaign - publication de core.Session

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"context"
-	"datapi/pkg/campaign"
+	"datapi/pkg/campaigns"
 	"datapi/pkg/core"
 	"datapi/pkg/db"
 	"datapi/pkg/logPersistence"
@@ -45,5 +45,6 @@ func initAndStartAPI(datapi *core.Datapi) {
 	core.AddEndpoint(router, "/ops/imports", imports.ConfigureEndpoint, core.AdminAuthMiddleware)
 	core.AddEndpoint(router, "/ops/refresh", refresh.ConfigureEndpoint, core.AdminAuthMiddleware)
 	core.AddEndpoint(router, "/campaign", campaign.ConfigureEndpoint)
+	core.AddEndpoint(router, "/campaign", campaigns.ConfigureEndpoint)
 	core.StartAPI(router)
 }
--- migrations/230525_0_create_campaigns.sql
+++ migrations/230525_0_create_campaigns.sql
@@ -1,36 +1,30 @@
-create extension hstore;
-
 create sequence campaign_id;
 create table campaign (
   id integer primary key default nextval('campaign_id'),
   libelle text,
   wekan_domain_regexp text,
-  date_end date,
-  date_create timestamp default current_timestamp
+  date_fin date,
+  date_creation timestamp
 );
 
 create sequence campaign_etablissement_id;
 create table campaign_etablissement (
-  id integer primary key default nextval('campaign_etablissement_id'),
+  id integer primary key default nextval('campaign_etablissement'),
   id_campaign integer,
   siret text,
-  metadata hstore,
+  data jsonb,
   constraint fk_campaign
     foreign key(id_campaign)
       references campaign(id)
 );
 
-create sequence campaign_etablissement_action_id;
-create table campaign_etablissement_action (
-  id integer primary key default nextval('campaign_etablissement_action_id'),
-  id_campaign_etablissement integer,
+create sequence campaign_etablissement_user_id;
+create table campaign_etablissement_user (
+  id integer primary key default nextval('campaign_etablissement_user_id'),
   username text,
-  date_action timestamp,
+  date_start timestamp,
+  date_end timestamp,
   action text,
-  metadata hstore,
-  constraint fk_campaign
-    foreign key(id_campaign_etablissement)
-      references campaign_etablissement(id)
+  data jsonb
 );
-
 
