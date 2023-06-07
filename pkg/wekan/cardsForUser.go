94f85430aba72cff4f66ba9ec2e4574af4d9fe6d
Christophe Ninucci <christophe-gitkraken@zbouboutchi.net>
2023-06-07 09:58:43 +0200

    add rows close()

--- pkg/wekan/cardsForUser.go
+++ pkg/wekan/cardsForUser.go
@@ -69,10 +69,10 @@ func selectSummariesWithSirets(
 	raisonSociale *string,
 ) (core.Summaries, error) {
 	rows, err := db.Query(ctx, SqlGetCards, roles, user.Username, sirets, zone, limit, raisonSocialeLike(raisonSociale))
-	defer rows.Close()
 	if err != nil {
 		return core.Summaries{}, err
 	}
+	defer rows.Close()
 
 	var summaries core.Summaries
 	for rows.Next() {
