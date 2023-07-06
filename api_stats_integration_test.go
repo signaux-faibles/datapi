bf04e73605c4894f38776065abd09c8437b4cb6f
Raphaël Squelbut <raphael.squelbut@shodo.io>
2023-07-06 11:01:08 +0200

    ajoute des handlers et des tests

--- api_stats_integration_test.go
+++ api_stats_integration_test.go
@@ -3,64 +3,64 @@
 package main
 
 import (
-  "bytes"
-  "compress/gzip"
-  _ "embed"
-  "encoding/csv"
-  "fmt"
-  "net/http"
-  "testing"
-  "time"
+	"bytes"
+	"compress/gzip"
+	_ "embed"
+	"encoding/csv"
+	"fmt"
+	"net/http"
+	"testing"
+	"time"
 
-  "github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"
 
-  "datapi/pkg/stats"
-  "datapi/pkg/test"
+	"datapi/pkg/stats"
+	"datapi/pkg/test"
 )
 
 func TestAPI_get_stats_since_5_days_ago(t *testing.T) {
-  ass := assert.New(t)
+	ass := assert.New(t)
 
-  // GIVEN
-  var numberOfDays = 10
-  _ = insertSomeLogsAtTime(tuTime.AddDate(0, 0, -numberOfDays-1))
-  same := insertSomeLogsAtTime(tuTime.AddDate(0, 0, -numberOfDays))
-  after := insertSomeLogsAtTime(tuTime.AddDate(0, 0, -numberOfDays+1))
+	// GIVEN
+	var numberOfDays = 10
+	_ = insertSomeLogsAtTime(tuTime.AddDate(0, 0, -numberOfDays-1))
+	same := insertSomeLogsAtTime(tuTime.AddDate(0, 0, -numberOfDays))
+	after := insertSomeLogsAtTime(tuTime.AddDate(0, 0, -numberOfDays+1))
 
-  path := fmt.Sprintf("/stats/since/%d/days", numberOfDays)
-  response := test.HTTPGet(t, path)
+	path := fmt.Sprintf("/stats/since/%d/days", numberOfDays)
+	response := test.HTTPGet(t, path)
 
-  ass.Equal(http.StatusOK, response.StatusCode)
-  body := test.GetBodyQuietly(response)
-  records, err := readGZippedCSV(body)
-  ass.NoError(err)
-  // on compare le nombre de lignes lues dans le fichier
-  // et le nombre de lignes insérés en base
-  ass.Equal(same+after, len(records))
+	ass.Equal(http.StatusOK, response.StatusCode)
+	body := test.GetBodyQuietly(response)
+	records, err := readGZippedCSV(body)
+	ass.NoError(err)
+	// on compare le nombre de lignes lues dans le fichier
+	// et le nombre de lignes insérés en base
+	ass.Equal(same+after, len(records))
 }
 
 func TestAPI_get_stats_on_2023_03_01(t *testing.T) {
-  ass := assert.New(t)
+	ass := assert.New(t)
 
-  // GIVEN
-  search := "2023-03-01"
-  nbDays := 3
-  dayToSelect, err := time.Parse(stats.DATE_FORMAT, search)
-  ass.NoError(err)
-  _ = insertSomeLogsAtTime(dayToSelect.Add(-1 * time.Millisecond))
-  betweenStart := insertSomeLogsAtTime(dayToSelect)
-  betweenEnd := insertSomeLogsAtTime(dayToSelect.AddDate(0, 0, nbDays))
-  _ = insertSomeLogsAtTime(dayToSelect.AddDate(0, 0, nbDays+1))
-  path := fmt.Sprintf("/stats/from/%s/for/%d/days", search, nbDays)
-  response := test.HTTPGet(t, path)
+	// GIVEN
+	search := "2023-03-01"
+	nbDays := 3
+	dayToSelect, err := time.Parse(stats.DATE_FORMAT, search)
+	ass.NoError(err)
+	_ = insertSomeLogsAtTime(dayToSelect.Add(-1 * time.Millisecond))
+	betweenStart := insertSomeLogsAtTime(dayToSelect)
+	betweenEnd := insertSomeLogsAtTime(dayToSelect.AddDate(0, 0, nbDays))
+	_ = insertSomeLogsAtTime(dayToSelect.AddDate(0, 0, nbDays+1))
+	path := fmt.Sprintf("/stats/from/%s/for/%d/days", search, nbDays)
+	response := test.HTTPGet(t, path)
 
-  ass.Equal(http.StatusOK, response.StatusCode)
-  body := test.GetBodyQuietly(response)
-  records, err := readGZippedCSV(body)
-  ass.NoError(err)
-  // on compare le nombre de lignes lues dans le fichier
-  // et le nombre de lignes insérés en base
-  ass.Equal(betweenStart+betweenEnd, len(records))
+	ass.Equal(http.StatusOK, response.StatusCode)
+	body := test.GetBodyQuietly(response)
+	records, err := readGZippedCSV(body)
+	ass.NoError(err)
+	// on compare le nombre de lignes lues dans le fichier
+	// et le nombre de lignes insérés en base
+	ass.Equal(betweenStart+betweenEnd, len(records))
 }
 
 //func TestAPI_get_stats_with_bad_format_in_param(t *testing.T) {
@@ -88,20 +88,20 @@ func TestAPI_get_stats_on_2023_03_01(t *testing.T) {
 //}
 
 func readGZippedCSV(body []byte) ([][]string, error) {
-  var data bytes.Buffer
-  _, err := data.Write(body)
-  if err != nil {
-    return nil, err
-  }
-  gzipReader, err := gzip.NewReader(&data)
-  if err != nil {
-    return nil, err
-  }
-  defer gzipReader.Close()
-  csvReader := csv.NewReader(gzipReader)
-  records, err := csvReader.ReadAll()
-  if err != nil {
-    return nil, err
-  }
-  return records, nil
+	var data bytes.Buffer
+	_, err := data.Write(body)
+	if err != nil {
+		return nil, err
+	}
+	gzipReader, err := gzip.NewReader(&data)
+	if err != nil {
+		return nil, err
+	}
+	defer gzipReader.Close()
+	csvReader := csv.NewReader(gzipReader)
+	records, err := csvReader.ReadAll()
+	if err != nil {
+		return nil, err
+	}
+	return records, nil
 }
