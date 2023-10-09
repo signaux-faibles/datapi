0d4ce578c5061c1a8746f503611af779db6d22f1
Raphaël Squelbut <raphael.squelbut@shodo.io>
2023-10-09 15:52:54 +0200

    déplace tout l'écriture du fichier excel dans un fichier excel.go

---
+++ pkg/stats/excel.go
@@ -0,0 +1,61 @@
+package stats
+
+import (
+	"io"
+	"log/slog"
+
+	"github.com/pkg/errors"
+	"github.com/xuri/excelize/v2"
+)
+
+func newExcel() *excelize.File {
+	return excelize.NewFile()
+}
+
+func createSheet(f *excelize.File, sheetName string, index int) (string, error) {
+	// Create a new sheet.
+	err := f.SetSheetName(f.GetSheetName(index), sheetName)
+	if err != nil {
+		return "", err
+	}
+	f.SetActiveSheet(index)
+	return sheetName, err
+}
+
+func writeActiviteToExcel(f *excelize.File, sheetName string, ligne activiteParUtilisateur, row int) error {
+	err := writeString(f, sheetName, ligne.username, 1, row)
+	if err != nil {
+		return errors.Wrap(err, "erreur pendant l'écriture du username")
+	}
+	err = writeString(f, sheetName, ligne.visites, 2, row)
+	if err != nil {
+		return errors.Wrap(err, "erreur pendant l'écriture du nombre de visites")
+	}
+	err = writeString(f, sheetName, ligne.actions, 3, row)
+	if err != nil {
+		return errors.Wrap(err, "erreur pendant l'écriture du nombre d'actions")
+	}
+	err = writeString(f, sheetName, ligne.segment, 4, row)
+	if err != nil {
+		return errors.Wrap(err, "erreur pendant l'écriture du nombre de segments")
+	}
+	return nil
+}
+
+func writeString(f *excelize.File, sheetName string, value string, col, row int) error {
+	cell, err := excelize.CoordinatesToCellName(col, row)
+	if err != nil {
+		return errors.Wrap(err, "erreur pendant la récupération des coordonnées")
+	}
+	err = f.SetCellStr(sheetName, cell, value)
+	return err
+}
+
+func exportTo(output io.Writer, excel *excelize.File) error {
+	_, err := excel.WriteTo(output, excelize.Options{RawCellValue: true})
+	if err != nil {
+		slog.Error("erreur pendant l'écriture du fichier excel")
+		return err
+	}
+	return nil
+}
