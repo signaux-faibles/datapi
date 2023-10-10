8d2f337643016fbb307b755a383cdf3d90ee54a0
Raphaël Squelbut <raphael.squelbut@shodo.io>
2023-10-10 11:28:03 +0200

    commit la correction que j'ai mis 2h à trouver

--- pkg/stats/utils.go
+++ pkg/stats/utils.go
@@ -97,7 +97,7 @@ func createCSV(archive *zip.Writer, filename string, results chan accessLog) err
 
 func createExcel(archive *zip.Writer, filename string, activites chan activiteParUtilisateur) error {
 	slog.Info("crée l'entrée excel", slog.String("filename", filename))
-	excelFile, err := archive.CreateRaw(&zip.FileHeader{
+	excelFile, err := archive.CreateHeader(&zip.FileHeader{
 		Name:     filename + ".xlsx",
 		Comment:  "fourni par Datapi avec dégoût",
 		NonUTF8:  false,
