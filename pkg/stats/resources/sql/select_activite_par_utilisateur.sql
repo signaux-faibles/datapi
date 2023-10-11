00fa646205856abb25b2ebbfda2a1ff2f35dd3a1
Raphaël Squelbut <raphael.squelbut@shodo.io>
2023-10-11 09:48:07 +0200

    corrige la requête SQL pour gérer les nuls

--- pkg/stats/resources/sql/select_activite_par_utilisateur.sql
+++ pkg/stats/resources/sql/select_activite_par_utilisateur.sql
@@ -1,10 +1,11 @@
-WITH temp_logs AS (SELECT date_bin('8 hours', date_add, TIMESTAMP '0001-01-01') AS session,
+WITH temp_logs AS (SELECT date_bin('8 hours', date_add, TIMESTAMP '0001-01-01')     AS session,
                           path,
                           method,
-                          v_log.tokencontent ->> 'email'::text                  as username,
-                          v_log.tokencontent ->> 'segment'::text                as segment
+                          COALESCE(tokencontent ->> 'email'::text, 'EMAIL INCONNU') as username,
+                          COALESCE(tokencontent ->> 'segment'::text, '-')           as segment
                    FROM v_log
-                   WHERE date_add >= $1 and date_add < $2)
+                   WHERE date_add >= $1
+                     and date_add < $2)
 SELECT username,
        count(distinct session) AS visites,
        count(path)             AS actions,
