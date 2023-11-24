4ae2361b08595b66b44829e4056664c68ad06700
Christophe Ninucci <christophe-gitkraken@zbouboutchi.net>
2023-11-24 14:16:10 +0100

    - transforme les vues matérialisées en tables

--- migrations/231120_0_delai_dans_summmaries.sql
+++ migrations/231120_0_delai_dans_summmaries.sql
@@ -1,5 +1,230 @@
 drop materialized view if exists v_summaries;
-create materialized view v_summaries as
+
+drop materialized view if exists v_roles;
+create table if not exists v_roles  as
+SELECT etablissement.siren,
+       array_agg(DISTINCT etablissement.departement) AS roles
+FROM etablissement
+WHERE etablissement.version = 0
+GROUP BY etablissement.siren;
+
+create index if not exists idx_v_roles_roles
+    on v_roles using gin (roles);
+
+create index if not exists idx_v_roles_siret
+    on v_roles (siren);
+
+drop materialized view if exists v_naf;
+create table if not exists v_naf as
+SELECT n1.id      AS id_n1,
+       n2.id      AS id_n2,
+       n3.id      AS id_n3,
+       n4.id      AS in_n4,
+       n5.id      AS id_n5,
+       n1.code    AS code_n1,
+       n2.code    AS code_n2,
+       n3.code    AS code_n3,
+       n4.code    AS code_n4,
+       n5.code    AS code_n5,
+       n1.libelle AS libelle_n1,
+       n2.libelle AS libelle_n2,
+       n3.libelle AS libelle_n3,
+       n4.libelle AS libelle_n4,
+       n5.libelle AS libelle_n5
+FROM naf n5
+         JOIN naf n4 ON n5.id_parent = n4.id
+         JOIN naf n3 ON n4.id_parent = n3.id
+         JOIN naf n2 ON n3.id_parent = n2.id
+         JOIN naf n1 ON n2.id_parent = n1.id;
+
+create index if not exists idx_v_naf_n1
+    on v_naf (code_n1);
+
+create index if not exists idx_v_naf_n5
+    on v_naf (code_n5);
+
+drop materialized view if exists v_last_procol;
+create table if not exists v_last_procol as
+SELECT etablissement_procol0.siren,
+       CASE
+           WHEN 'liquidation'::text = ANY (array_agg(etablissement_procol0.action_procol)) THEN 'liquidation'::text
+           WHEN 'redressement'::text = ANY (array_agg(etablissement_procol0.action_procol)) THEN
+               CASE
+                   WHEN 'redressement_plan_continuation'::text = ANY
+                        (array_agg((etablissement_procol0.action_procol || '_'::text) ||
+                                   etablissement_procol0.stade_procol)) THEN 'plan_continuation'::text
+                   ELSE 'redressement'::text
+                   END
+           WHEN 'sauvegarde'::text = ANY (array_agg(etablissement_procol0.action_procol)) THEN
+               CASE
+                   WHEN 'sauvegarde_plan_continuation'::text = ANY
+                        (array_agg((etablissement_procol0.action_procol || '_'::text) ||
+                                   etablissement_procol0.stade_procol)) THEN 'plan_sauvegarde'::text
+                   ELSE 'sauvegarde'::text
+                   END
+           ELSE NULL::text
+           END                               AS last_procol,
+       max(etablissement_procol0.date_effet) AS date_effet
+FROM etablissement_procol0
+GROUP BY etablissement_procol0.siren;
+
+create unique index if not exists idx_v_last_procol_siren
+    on v_last_procol (siren);
+
+
+
+drop materialized view if exists v_last_effectif;
+create table if not exists v_last_effectif as
+WITH last_effectif_entreprise AS (SELECT etablissement_periode_urssaf0.siren,
+                                         last(etablissement_periode_urssaf0.periode
+                                              ORDER BY etablissement_periode_urssaf0.periode) AS periode
+                                  FROM etablissement_periode_urssaf0
+                                  WHERE etablissement_periode_urssaf0.effectif IS NOT NULL
+                                  GROUP BY etablissement_periode_urssaf0.siren)
+SELECT e.siret,
+       e.siren,
+       e.effectif,
+       e.periode
+FROM etablissement_periode_urssaf0 e
+         JOIN last_effectif_entreprise l ON e.siren::text = l.siren::text AND e.periode = l.periode;
+
+drop materialized view if exists v_hausse_urssaf;
+create table if not exists v_hausse_urssaf as
+SELECT etablissement_periode_urssaf0.siret,
+       (array_agg(etablissement_periode_urssaf0.part_patronale + etablissement_periode_urssaf0.part_salariale
+                  ORDER BY etablissement_periode_urssaf0.periode DESC))[0:3] AS dette,
+       CASE
+           WHEN first(etablissement_periode_urssaf0.part_salariale
+                      ORDER BY etablissement_periode_urssaf0.periode DESC) > 100::double precision THEN true
+           ELSE false
+           END                                                               AS presence_part_salariale,
+       max(etablissement_periode_urssaf0.periode)                            AS periode_urssaf
+FROM etablissement_periode_urssaf0
+GROUP BY etablissement_periode_urssaf0.siret;
+
+create unique index if not exists idx_v_hausse_urssaf_siret
+    on v_hausse_urssaf (siret);
+
+
+drop materialized view if exists v_etablissement_raison_sociale;
+create table if not exists v_etablissement_raison_sociale as
+SELECT et.id AS id_etablissement,
+       en.id AS id_entreprise,
+       en.raison_sociale,
+       et.siret
+FROM entreprise0 en
+         JOIN etablissement0 et ON en.siren::text = et.siren::text;
+
+create index if not exists idx_entreprise_raison_sociale
+    on v_etablissement_raison_sociale using gin (siret gin_trgm_ops, raison_sociale gin_trgm_ops);
+
+drop materialized view if exists v_diane_variation_ca;
+create table if not exists v_diane_variation_ca as
+WITH diane AS (SELECT entreprise_diane0.siren,
+                      entreprise_diane0.arrete_bilan_diane,
+                      entreprise_diane0.exercice_diane::integer                                                 AS exercice_diane,
+                      lag(entreprise_diane0.arrete_bilan_diane, 1)
+                      OVER (PARTITION BY entreprise_diane0.siren ORDER BY entreprise_diane0.arrete_bilan_diane) AS prev_arrete_bilan_diane,
+                      entreprise_diane0.chiffre_affaire,
+                      lag(entreprise_diane0.chiffre_affaire, 1)
+                      OVER (PARTITION BY entreprise_diane0.siren ORDER BY entreprise_diane0.arrete_bilan_diane) AS prev_chiffre_affaire,
+                      entreprise_diane0.resultat_expl,
+                      lag(entreprise_diane0.resultat_expl, 1)
+                      OVER (PARTITION BY entreprise_diane0.siren ORDER BY entreprise_diane0.arrete_bilan_diane) AS prev_resultat_expl,
+                      entreprise_diane0.excedent_brut_d_exploitation,
+                      lag(entreprise_diane0.excedent_brut_d_exploitation, 1)
+                      OVER (PARTITION BY entreprise_diane0.siren ORDER BY entreprise_diane0.arrete_bilan_diane) AS prev_excedent_brut_d_exploitation
+               FROM entreprise_diane0
+               ORDER BY entreprise_diane0.siren, entreprise_diane0.arrete_bilan_diane DESC)
+SELECT diane.siren,
+       first(diane.arrete_bilan_diane ORDER BY diane.arrete_bilan_diane DESC)                AS arrete_bilan,
+       first(diane.exercice_diane ORDER BY diane.arrete_bilan_diane DESC)                    AS exercice_diane,
+       first(diane.prev_chiffre_affaire ORDER BY diane.arrete_bilan_diane DESC)              AS prev_chiffre_affaire,
+       first(diane.chiffre_affaire ORDER BY diane.arrete_bilan_diane DESC)                   AS chiffre_affaire,
+       first(
+               CASE
+                   WHEN diane.prev_chiffre_affaire <> 0::double precision
+                       THEN diane.chiffre_affaire / diane.prev_chiffre_affaire
+                   ELSE NULL::real
+                   END ORDER BY diane.arrete_bilan_diane DESC)                               AS variation_ca,
+       first(diane.resultat_expl ORDER BY diane.arrete_bilan_diane DESC)                     AS resultat_expl,
+       first(diane.prev_resultat_expl ORDER BY diane.arrete_bilan_diane DESC)                AS prev_resultat_expl,
+       first(diane.excedent_brut_d_exploitation
+             ORDER BY diane.arrete_bilan_diane DESC)                                         AS excedent_brut_d_exploitation,
+       first(diane.prev_excedent_brut_d_exploitation
+             ORDER BY diane.arrete_bilan_diane DESC)                                         AS prev_excedent_brut_d_exploitation
+FROM diane
+GROUP BY diane.siren;
+
+create unique index if not exists idx_v_diane_variation_ca_siren
+    on v_diane_variation_ca (siren);
+
+drop materialized view if exists v_apdemande;
+create table if not exists v_apdemande as
+SELECT e.siret,
+       COALESCE((SELECT DISTINCT true AS bool
+                 FROM etablissement_apdemande0
+                 WHERE etablissement_apdemande0.siret::text = e.siret::text
+                   AND (etablissement_apdemande0.periode_end + '1 year'::interval) >=
+                       date_trunc('month'::text, CURRENT_TIMESTAMP)), false)      AS ap,
+       (sum(c.heure_consomme ORDER BY c.periode) / 12::double precision)::integer AS heure_consomme,
+       (sum(c.montant ORDER BY c.periode) / 12::double precision)::integer        AS montant
+FROM etablissement0 e
+         LEFT JOIN etablissement_apconso0 c ON c.siret::text = e.siret::text AND (c.periode + '1 year'::interval) >=
+                                                                                 date_trunc('month'::text, CURRENT_TIMESTAMP)
+GROUP BY e.siret;
+
+create unique index if not exists idx_v_apdemande_siret
+    on v_apdemande (siret);
+
+
+drop materialized view if exists v_alert_etablissement;
+create table if not exists v_alert_etablissement as
+WITH lists AS (SELECT score0.siret,
+                      first(score0.libelle_liste ORDER BY score0.batch) AS first_list,
+                      last(score0.libelle_liste ORDER BY score0.batch)  AS last_list,
+                      first(score0.alert ORDER BY score0.batch DESC)    AS last_alert
+               FROM score0
+               WHERE score0.alert = ANY (ARRAY ['Alerte seuil F1'::text, 'Alerte seuil F2'::text])
+               GROUP BY score0.siret),
+     red_lists AS (SELECT score0.siret,
+                          first(score0.libelle_liste ORDER BY score0.batch) AS first_red_list
+                   FROM score0
+                   WHERE score0.alert = 'Alerte seuil F1'::text
+                   GROUP BY score0.siret)
+SELECT l.siret,
+       l.first_list,
+       l.last_list,
+       l.last_alert,
+       rl.first_red_list
+FROM lists l
+         LEFT JOIN red_lists rl ON rl.siret = l.siret;
+
+create unique index if not exists idx_v_alert_etablissement_siret
+    on v_alert_etablissement (siret);
+
+
+drop materialized view if exists v_alert_entreprise;
+create table if not exists v_alert_entreprise as
+WITH first_lists AS (SELECT DISTINCT sc.siren,
+                                     first(sc.libelle_liste) OVER (window_siren)                           AS first_list,
+                                     first(sc.libelle_liste)
+                                     FILTER (WHERE sc.alert = 'Alerte seuil F1'::text) OVER (window_siren) AS first_red_list
+                     FROM score0 sc
+                     WHERE sc.alert = ANY (ARRAY ['Alerte seuil F1'::text, 'Alerte seuil F2'::text])
+                     WINDOW window_siren AS (PARTITION BY sc.siren ORDER BY sc.batch))
+SELECT first_lists.siren,
+       max(first_lists.first_list)     AS first_list,
+       max(first_lists.first_red_list) AS first_red_list
+FROM first_lists
+WHERE first_lists.first_list IS NOT NULL
+GROUP BY first_lists.siren;
+
+create index if not exists idx_v_alert_entreprise_siren
+    on v_alert_entreprise (siren);
+
+
+create table if not exists v_summaries as
 WITH last_liste AS (SELECT first(liste.libelle ORDER BY liste.batch DESC, liste.algo DESC) AS last_liste,
                            first('20' || substring(batch from 1 for 2) || '-' || substring(batch from 3 for 2) || '-01'
                                  ORDER BY liste.batch DESC, liste.algo DESC
@@ -97,25 +322,23 @@ FROM last_liste l
          LEFT JOIN categorie_juridique cj2 ON "substring"(cj.code, 0, 3) = cj2.code
          LEFT JOIN categorie_juridique cj1 ON "substring"(cj.code, 0, 2) = cj1.code;
 
-alter materialized view v_summaries owner to postgres;
-
-create index idx_v_summaries_order_raison_sociale
+create index if not exists idx_v_summaries_order_raison_sociale
     on v_summaries (raison_sociale, siret);
 
-create index idx_v_summaries_raison_sociale
+create index if not exists idx_v_summaries_raison_sociale
     on v_summaries using gin (siret gin_trgm_ops, raison_sociale gin_trgm_ops);
 
-create index idx_v_summaries_roles
+create index if not exists idx_v_summaries_roles
     on v_summaries using gin (roles);
 
-create index idx_v_summaries_score
+create index if not exists idx_v_summaries_score
     on v_summaries (valeur_score desc, siret asc, siege asc, effectif asc, effectif_entreprise asc, code_departement asc,
                     last_procol asc, chiffre_affaire asc, etat_administratif asc, first_alert asc, first_list_entreprise
                     asc, first_red_list_entreprise asc, has_delai asc)
     where (alert <> 'Pas d''alerte'::text);
 
-create index idx_v_summaries_siren
+create index if not exists idx_v_summaries_siren
     on v_summaries (siren);
 
-create index idx_v_summaries_siret
+create index if not exists idx_v_summaries_siret
     on v_summaries (siret);
