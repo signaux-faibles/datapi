1fa4db8cb4c5068eaeb14dd04b218ee4f10770be
Christophe Ninucci <christophe-gitkraken@zbouboutchi.net>
2024-01-15 11:28:11 +0100

    - prise en compte des bons établissements (cette fois)

--- pkg/ops/campaignops/sql/siretsFromListe.sql
+++ pkg/ops/campaignops/sql/siretsFromListe.sql
@@ -1,6 +1,6 @@
 select siret
 from v_summaries
 where last_list=$1 and alert != 'Pas d''alerte'
-  and last_procol in ('in_bonis', 'redressement', 'plan_redressement')
+  and last_procol in ('in_bonis', 'continuation', 'sauvegarde', 'plan_sauvegarde')
   and etat_administratif != 'F'
   and first_alert;
