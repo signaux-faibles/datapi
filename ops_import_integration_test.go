8bc9c8adaf874f837b1db032bf9e3e9917a37aaf
Raphaël Squelbut <raphael.squelbut@shodo.io>
2024-02-21 19:12:15 +0100

    ajoute un test API pour l'aggregation URSSAF

--- ops_import_integration_test.go
+++ ops_import_integration_test.go
@@ -17,6 +17,33 @@ import (
 	"datapi/pkg/test"
 )
 
+func TestApi_OpsImports_UrssafAggregate(t *testing.T) {
+	ass := assert.New(t)
+	path := "/ops/imports/urssaf/aggregate"
+	response := test.HTTPGet(t, path)
+
+	ass.Equalf(http.StatusOK, response.StatusCode, "body de la réponse : %s", test.GetBodyQuietly(response))
+}
+
+func Test_UrssafAgregate(t *testing.T) {
+	//utils.ConfigureLogLevel("warn")
+	ass := assert.New(t)
+	ctx := context.Background()
+
+	current := scripts.StartRefreshScript(ctx, db.Get(), imports.ExecuteAggregationURSSAF)
+
+	actual := scripts.Refresh{}
+	var err error
+	for actual.Status != scripts.Failed {
+		actual, err = scripts.Fetch(current.UUID)
+		require.NoError(t, err)
+		t.Log(actual)
+	}
+
+	ass.Equal(current.UUID, actual.UUID)
+	ass.Equal(scripts.Failed, actual.Status) // -> l'import des données URSSAF n'a pas eu lieu, l'erreur est normale
+}
+
 func TestApi_OpsImports_PredictionsRefresh(t *testing.T) {
 	ass := assert.New(t)
 	path := "/ops/imports/liste/refresh"
