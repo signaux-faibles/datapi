24bc2bbfe08a30cb05acce3fff5abd52b9d3c510
Charles Marcoin <charles.marcoin@gmail.com>
2024-05-15 16:51:00 +0200

    exclude companies created recently

--- pkg/core/sql.go
+++ pkg/core/sql.go
@@ -92,6 +92,7 @@ from v_summaries s
   and (s.etat_administratif = $21 or $21 is null)
   and (s.first_alert = $22 or $22 is null)
   and (not (s.has_delai = $23) or $23 is null)
+  and (s.date_creation_entreprise <= $24 or $24 is null)
 order by s.alert, s.valeur_score desc, s.siret
 limit $2 offset $3`
 
@@ -129,9 +130,9 @@ func (p summaryParams) toSQLScoreParams() []interface{} {
 		p.etatAdministratif,
 		p.firstAlert,
 		p.hasntDelai,
+		p.creationDateThreshold,
 		// insert new argument before and manually update index of libelleListe
 		p.libelleListe,
-		p.creationDateThreshold,
 	}
 }
 
@@ -188,7 +189,7 @@ var sqlScore = `select
   and (n.code_n1 = any($15) or $15 is null)
   and (s.etat_administratif = $21 or $21 is null)
   and (not (s.has_delai = $23) or $23 is null)
-  and ((s.first_list_etablissement = $24 or s.first_red_list_etablissement = $24) and $22 or $22 is null)
-  and (s.create_date >= $25 or $25 is null)
+  and (s.date_creation_entreprise <= $24 or $24 is null)
+  and ((s.first_list_etablissement = $25 or s.first_red_list_etablissement = $25) and $22 or $22 is null)
   order by sc.alert, sc.score desc, s.siret
   limit $2 offset $3`
