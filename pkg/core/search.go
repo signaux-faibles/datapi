2d4b0a63bb5a785131357b068f9a3b1ad5239d8c
Charles Marcoin <charles.marcoin@gmail.com>
2024-05-15 16:17:23 +0200

    fix params in pkg core

--- pkg/core/search.go
+++ pkg/core/search.go
@@ -2,6 +2,7 @@ package core
 
 import (
 	"datapi/pkg/utils"
+
 	"github.com/gin-gonic/gin"
 	"github.com/spf13/viper"
 )
@@ -88,7 +89,7 @@ func searchEtablissement(params searchParams) (searchResult, utils.Jerror) {
 		zoneGeo, &limit, &offset, &liste[0].ID, false, &params.Search, &params.IgnoreRoles, &params.IgnoreZone,
 		params.username, params.SiegeUniquement, "raison_sociale", &False, nil, params.Departements, nil,
 		params.EffectifMin, nil, nil, params.Activites, params.EffectifMinEntreprise, params.EffectifMaxEntreprise,
-		params.CaMin, params.CaMax, params.ExcludeSecteursCovid, params.EtatAdministratif, nil, nil,
+		params.CaMin, params.CaMax, params.ExcludeSecteursCovid, params.EtatAdministratif, nil, nil, nil,
 	}
 	summaries, err := getSummaries(summaryparams)
 	if err != nil {
