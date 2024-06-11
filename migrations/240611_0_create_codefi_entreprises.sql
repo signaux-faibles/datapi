41cfc391e04e8bc8db1ef4552f2c68e29ce9af88
Charles Marcoin <charles.marcoin@gmail.com>
2024-06-11 14:13:33 +0200

    added a migration to create a codefi enterprises table

---
+++ migrations/240611_0_create_codefi_entreprises.sql
@@ -0,0 +1,12 @@
+create sequence codefi_entreprise_id;
+create table codefi_entreprises (
+  id int primary key default nextval('codefi_entreprise_id'),
+  libelle text,
+  siren text,
+  code_departement text,
+  date_add timestamp default current_timestamp
+);
+
+create index idx_codefi_entreprises_siren on codefi_entreprises (siren);
+
+create index idx_codefi_entreprises_siren_libelle on codefi_entreprises (siren, libelle);
