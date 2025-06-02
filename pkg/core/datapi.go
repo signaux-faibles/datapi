0f2de414b1ac19e71ecfc9409a2e340d8959d302
Josquin Cornec <josquin.cornec@beta.gouv.fr>
2025-06-02 11:04:34 +0200

    ajoute un logger personnalisé

--- pkg/core/datapi.go
+++ pkg/core/datapi.go
@@ -2,11 +2,17 @@
 package core
 
 import (
+	"bytes"
+	"encoding/json"
 	"flag"
 	"fmt"
+	"github.com/golang-jwt/jwt/v4"
+	"io"
 	"log"
 	"log/slog"
 	"net/http"
+	"strings"
+	"time"
 
 	"github.com/Nerzal/gocloak/v10"
 	"github.com/gin-contrib/cors"
@@ -61,6 +67,7 @@ func PrepareDatapi(kanbanService KanbanService, saver AccessLogSaver) (*Datapi, 
 
 // InitAPI initialise l'api
 func (datapi *Datapi) InitAPI(router *gin.Engine) {
+	router.Use(gin.Recovery())
 	config := cors.DefaultConfig()
 	config.AllowOrigins = viper.GetStringSlice("corsAllowOrigins")
 	config.AddExposeHeaders("Content-Disposition", "responseType", "Content-Type", "Cache-Control", "Connection", "Transfer-Encoding", "X-Accel-Buffering")
@@ -68,7 +75,16 @@ func (datapi *Datapi) InitAPI(router *gin.Engine) {
 	config.AddAllowHeaders("Authorization", "responseType")
 	config.AddAllowMethods("GET", "POST", "DELETE")
 	router.Use(cors.New(config))
-	err := router.SetTrustedProxies(nil)
+	const (
+		IP_REVERSE_PROXY       string = "10.0.2.100"
+		IP_REVERSE_PROXY_ADMIN string = "10.3.0.195"
+	)
+	err := router.SetTrustedProxies([]string{IP_REVERSE_PROXY, IP_REVERSE_PROXY_ADMIN})
+
+	router.Use(ExtractEmailFromJWT())
+	router.Use(ExtractBodyFromHttpJson())
+	router.Use(CustomLogger())
+
 	if err != nil {
 		panic(err.Error())
 	}
@@ -111,6 +127,77 @@ func (datapi *Datapi) InitAPI(router *gin.Engine) {
 	reference.GET("/regions", regionsHandler)
 
 	configureKanbanEndpoint("/kanban", router, AuthMiddleware(), datapi.LogMiddleware)
+}
+
+func CustomLogger() gin.HandlerFunc {
+	return gin.LoggerWithFormatter(func(param gin.LogFormatterParams) string {
+		skipPaths := map[string]bool{
+			"/healthcheck":       true,
+			"/ops/utils/metrics": true,
+		}
+		if skipPaths[param.Path] {
+			return ""
+		}
+		email := param.Keys["email"]
+		body := param.Keys["body"]
+		return fmt.Sprintf("[GIN]\t%s\t|%d\t|%s\t|%s\t|%v\t|%s %s%s\n",
+			param.TimeStamp.Format(time.DateTime),
+			param.StatusCode,
+			param.Latency,
+			param.ClientIP,
+			email,
+			param.Method,
+			param.Path,
+			body,
+		)
+	})
+}
+
+func ExtractEmailFromJWT() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		auth := c.GetHeader("Authorization")
+		if strings.HasPrefix(auth, "Bearer ") {
+			tokenStr := strings.TrimPrefix(auth, "Bearer ")
+			parser := jwt.NewParser()
+			token, _, err := parser.ParseUnverified(tokenStr, jwt.MapClaims{})
+			if err == nil {
+				if claims, ok := token.Claims.(jwt.MapClaims); ok {
+					if email, exists := claims["email"]; exists {
+						c.Set("email", email)
+					}
+				}
+			}
+		}
+		c.Next()
+	}
+}
+
+func ExtractBodyFromHttpJson() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		body := ""
+		if c.Request.Method == "POST" || c.Request.Method == "PUT" {
+			bodyBytes, _ := io.ReadAll(c.Request.Body)
+			c.Request.Body = io.NopCloser(bytes.NewBuffer(bodyBytes)) // recharge le body inchangé
+
+			// transforme ["A", "B", "C"] en "[A, B, C]" pour la lisibilité des logs
+			var obj map[string]interface{}
+			if err := json.Unmarshal(bodyBytes, &obj); err == nil {
+				for k, v := range obj {
+					if arr, ok := v.([]interface{}); ok {
+						strs := make([]string, len(arr))
+						for i, val := range arr {
+							strs[i] = fmt.Sprintf("%v", val)
+						}
+						obj[k] = "[" + strings.Join(strs, ", ") + "]"
+					}
+				}
+				pretty, _ := json.MarshalIndent(obj, "", "  ")
+				body = string(pretty)
+			}
+		}
+		c.Set("body", body)
+		c.Next()
+	}
 }
 
 // AddEndpoint permet de rajouter un endpoint au niveau de l'API
